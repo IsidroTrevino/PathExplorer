47f0e11f8ce973d041060d96da499122630073e5
debbelzz <A00835550@tec.mx>
2025-04-01 00:27:06 -0600

    front basic info

--- pathexplorer/src/app/user/basic-info/page.tsx
+++ pathexplorer/src/app/user/basic-info/page.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { SideBar, UserRole } from '@/components/GlobalComponents/SlideBar';
+import { Input } from '@/components/ui/input';
 
 export default function BasicInfoPage() {
   return (
@@ -11,6 +12,42 @@ export default function BasicInfoPage() {
       <div className="flex-1 p-8">
         <h1 className="text-2xl font-bold">Welcome back, Isidro Trevino!</h1>
         <p>Fill in and verify your personal information, you can modify it whenever you want.</p>
+
+        <div className="pt-6 pr-96 flex-col space-y-6">
+
+          <div className='flex items-center gap-40'>
+            <h1 className='text-l font-semibold w-[300px]'>Name</h1>
+            <Input type="text" placeholder='Full name'></Input>
+          </div>
+
+          <div className='flex items-center gap-40'>
+            <h1 className='text-l font-semibold w-[300px]'>Personal e-mail</h1>
+            <Input type="text" placeholder='Personal mail'></Input>
+          </div>
+
+          <div className='flex items-center gap-40'>
+            <h1 className='text-l font-semibold  w-[300px]'>Company e-mail</h1>
+            <Input type="text" placeholder='Company mail'></Input>
+          </div>
+
+          <div className='flex items-center gap-40'>
+            <h1 className='text-l font-semibold  w-[300px]'>Phone number</h1>
+            <Input type="text" placeholder='Phone number'></Input>
+          </div>
+
+          <div className='flex items-center gap-40'>
+            <h1 className='text-l font-semibold  w-[300px]'>Department/Role</h1>
+            <Input type="text" placeholder='Department/role'></Input>
+          </div>
+
+          <div className='flex items-center gap-40'>
+            <h1 className='text-l font-bold  w-[300px]'>Assignment percentage</h1>
+            <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+              <div className="bg-[#A055F5] text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{width: '38%'}}> 38%</div>
+            </div>
+          </div>
+
+        </div>
       </div>
     </div>
   );
