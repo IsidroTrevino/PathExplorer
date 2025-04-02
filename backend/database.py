0ce01644144ab7b03be4d57cd2777eeceb7561da
AyanariuZ <alejandro96.mia@gmail.com>
2025-04-02 16:04:27 -0600

    Update .gitignore

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+from config import settings
 
 try:
+<<<<<<< HEAD
+    engine = create_engine(settings.DATABASE_URL)
+=======
     engine = create_engine("postgresql://adminpathexplorer:amoaleo<3@pathexplorer.postgres.database.azure.com:5432/pathexplorer?sslmode=require")
+>>>>>>> parent of cee7673 (API v1)
 except Exception as e:
     print(f"Error: {e}")
 
