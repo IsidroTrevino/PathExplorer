1d5bc78575e3ede920ae4060366f0ff32bd06c07
IsidroTrevino <A00837104@tec.mx>
2025-05-20 22:22:42 -0600

    feat: update end-to-end test configuration and rename test files for clarity and test separation

--- pathexplorer/jest.e2e.config.js
+++ pathexplorer/jest.e2e.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: 'jest-puppeteer',
-  testMatch: ['**/tests/e2e/**/*.test.{js,ts,tsx}', '**/src/tests/e2e/**/*.test.{js,ts,tsx}'],
+  testMatch: ['**/tests/e2e/**/*.test.e2e.{js,ts,tsx}', '**/src/tests/e2e/**/*.test.e2e.{js,ts,tsx}'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest'
   },
