a33fa80300e33d942058a00c27aa4d19e36c74f1
IsidroTrevino <A00837104@tec.mx>
2025-06-06 00:49:24 -0600

    Refactored employees page

--- pathexplorer/src/app/user/employees/page.tsx
+++ pathexplorer/src/app/user/employees/page.tsx
@@ -1,50 +1,7 @@
-'use client';
-
-import { useState } from 'react';
 import { PageHeader } from '@/components/pageHeader';
-import { EmployeeTable } from '@/components/employeeTable';
-import { useGetEmployees } from '@/app/user/employees/hooks/useGetEmployees';
+import { Employees } from './components/Employees';
 
 export default function EmployeesPage() {
-  const [currentPage, setCurrentPage] = useState(1);
-  const [pageSize] = useState(20);
-  const [roleFilter, setRoleFilter] = useState<string | null>(null);
-  const [alphabetical, setAlphabetical] = useState<boolean | null>(null);
-  const [searchTerm, setSearchTerm] = useState<string | null>(null);
-
-  const {
-    data: employees,
-    totalPages,
-    loading,
-    error,
-  } = useGetEmployees({
-    page: currentPage,
-    size: pageSize,
-    role: roleFilter,
-    alphabetical,
-    search: searchTerm,
-    assigned: null,
-  });
-
-  const handlePageChange = (page: number) => {
-    setCurrentPage(page);
-  };
-
-  const handleRoleFilter = (role: string | null) => {
-    setRoleFilter(role);
-    setCurrentPage(1);
-  };
-
-  const handleSort = (isAlphabetical: boolean | null) => {
-    setAlphabetical(isAlphabetical);
-    setCurrentPage(1);
-  };
-
-  const handleSearch = (search: string | null) => {
-    setSearchTerm(search);
-    setCurrentPage(1);
-  };
-
   return (
     <div className="flex-1 overflow-y-auto">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
@@ -54,25 +11,7 @@ export default function EmployeesPage() {
         />
 
         <div className="mt-8">
-          {error ? (
-            <div className="rounded-md bg-red-50 p-4 text-red-700">
-              {error}
-            </div>
-          ) : (
-            <div className="h-[calc(100vh-250px)] overflow-auto">
-              <EmployeeTable
-                data={loading ? [] : employees}
-                loading={loading}
-                onPageChange={handlePageChange}
-                currentPage={currentPage}
-                totalPages={totalPages}
-                onRoleFilter={handleRoleFilter}
-                onSort={handleSort}
-                onSearch={handleSearch}
-                isExternalPagination={true}
-              />
-            </div>
-          )}
+          <Employees />
         </div>
       </div>
     </div>
